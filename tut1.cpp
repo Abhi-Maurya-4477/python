19b9121cba80ee8a3b31482efe77fa71cb359de6
Abhi-Maurya-4477 <abhishekkumar99169@gmail.com>
2024-09-23 20:29:29 +0530

    Add files via upload

---
+++ tut1.cpp
@@ -0,0 +1,27 @@
+// display ASCII value of any chracter
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+// char c;
+// cout<<"enter any chracter"<<endl;
+// cin>>c;
+// cout<<"the value of ASCII is  "<<c<< "is " <<int(c)<<endl;
+// return 0;
+// }
+
+// ->>>  sum of two numbers.....
+#include<iostream>
+using namespace std;
+int main(){
+int a,b;
+cout<<"enter the first number"<<endl;
+cin>>a;
+cout<<"enter the second number"<<endl;
+cin>>b;
+int sum = a+b;
+int munt = a*b;
+cout<<"the sum of a and b is " <<sum<<endl;
+cout<<"the muntiply of a and b is "<<munt<<endl;
+
+}
